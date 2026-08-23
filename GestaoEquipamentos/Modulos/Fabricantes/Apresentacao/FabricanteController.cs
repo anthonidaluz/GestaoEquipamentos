9ef006486d911f6179fb6abf407fdfd09001decc
anthonidaluz <anthonidaluz@gmail.com>
2026-08-23 16:08:08 -0300

    Adicionado os atributos do fabricante

---
+++ GestaoEquipamentos/Modulos/Fabricantes/Apresentacao/FabricanteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoEquipamentos.Modulos.Fabricantes.Apresentacao
+{
+    public class FabricanteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GestaoEquipamentos/Modulos/Fabricantes/Apresentacao/FabricanteViewModels.cs
@@ -0,0 +1,6 @@
+﻿namespace GestaoEquipamentos.Modulos.Fabricantes.Apresentacao
+{
+    public class FabricanteViewModels
+    {
+    }
+}

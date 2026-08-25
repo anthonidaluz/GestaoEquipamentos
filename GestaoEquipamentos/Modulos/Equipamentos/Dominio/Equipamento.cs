b288172640dec0eda9f8e3d8efc53e4ff133da13
anthonidaluz <anthonidaluz@gmail.com>
2026-08-25 19:22:07 -0300

    Adicionado atributos no dominio Equipamento

---
+++ GestaoEquipamentos/Modulos/Equipamentos/Dominio/Equipamento.cs
@@ -0,0 +1,23 @@
+﻿using GestaoEquipamentos.Compartilhado.Dominio;
+using System.Runtime.CompilerServices;
+
+namespace GestaoEquipamentos.Modulos.Equipamentos.Dominio
+{
+    public sealed class Equipamento : EntidadeBase
+    {
+        public string Nome { get; set; } = string.Empty;
+        public decimal PrecoAquisicao { get; set; }
+        public DateTime DataFabricacao { get; set; }
+        public Fabricante Fabricante { get; set; } = null!;
+
+        public override void Atualizar(EntidadeBase entidadeAtualizada)
+        {
+            Equipamento equipamentoAtualizado = (Equipamento)entidadeAtualizada;
+
+            Nome = equipamentoAtualizado.Nome;
+            PrecoAquisicao = equipamentoAtualizado.PrecoAquisicao;
+            DataFabricacao = equipamentoAtualizado.DataFabricacao;
+            Fabricante = equipamentoAtualizado.Fabricante;
+        }
+    }
+}

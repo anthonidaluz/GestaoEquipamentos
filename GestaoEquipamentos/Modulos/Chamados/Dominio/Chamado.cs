7f71563653fd1f993cb260eddbf29f78f3ae6a35
anthonidaluz <anthonidaluz@gmail.com>
2026-08-29 22:50:25 -0300

    Criado dominio base para modulos de chamados

---
+++ GestaoEquipamentos/Modulos/Chamados/Dominio/Chamado.cs
@@ -0,0 +1,31 @@
+﻿using GestaoEquipamentos.Modulos.Equipamentos.Dominio;
+
+namespace GestaoDeEquipamentos.WebApp.Modulos.Chamados.Dominio;
+
+public class Chamado
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; } = string.Empty;
+    public string Descricao { get; set; } = string.Empty;
+    public DateTime DataAbertura { get; set; }
+    public int EquipamentoId { get; set; }
+    public Equipamento? Equipamento { get; set; }
+    public int DiasEmAberto
+    {
+        get
+        {
+            TimeSpan tempoAberto = DateTime.Now.Date - DataAbertura.Date;
+            return tempoAberto.Days;
+        }
+    }
+    public Chamado()
+    {
+    }
+    public Chamado(string titulo, string descricao, DateTime dataAbertura, int equipamentoId)
+    {
+        Titulo = titulo;
+        Descricao = descricao;
+        DataAbertura = dataAbertura;
+        EquipamentoId = equipamentoId;
+    }
+}
